b1bd99f5f587114eebdf3209b05f8a527f3b7239
rocio picaza <rocio.picaza@gmail.com>
2021-01-23 15:22:30 -0300

    remove the new route for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :flats, only: [ :index, :show, :create, :new ] do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :create ]
   end
 end
