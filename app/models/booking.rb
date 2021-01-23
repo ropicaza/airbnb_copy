e813bcb034c614b8cf4d9d888f8c519c9afb0f95
tomas agnese <tagnese1985@gmail.com>
2021-01-23 16:11:45 -0300

    models and seed solved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :flat
 
 
-  validates :start_date, presence :true
-  validates :end_date, presence :true
-  validates :status, presence :true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,8 +2,8 @@ class Flat < ApplicationRecord
   belongs_to :user
 
 
-  validates :name, presence :true, uniqueness: true
-  validates :description, presence :true, length: { minimum: 50 }
-  validates :price, presence :true, :numericality => { :greater_than => 0 }
-  validates :location, presence :true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 1 }
+  validates :price, presence: true, :numericality => { :greater_than => 0 }
+  validates :location, presence: true
 end
