063e5e570d50af2dc8c98db0ec6f020b0d24abdd
tomas agnese <tagnese1985@gmail.com>
2021-01-23 14:51:49 -0300

    typo fix

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,7 +2,7 @@ class Flat < ApplicationRecord
   belongs_to :user
 
 
-  validates :name, presence :true, uniqueness: :true
+  validates :name, presence :true, uniqueness: true
   validates :description, presence :true, length: { minimum: 50 }
   validates :price, presence :true, :numericality => { :greater_than => 0 }
   validates :location, presence :true
