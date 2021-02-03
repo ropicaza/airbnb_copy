6cf54411b377bbff4054755f9a201150a092ea0a
tomas agnese <tagnese1985@gmail.com>
2021-02-03 17:21:14 -0300

    renter can review flat

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :renter, foreign_key: "user_id", class_name: "User"
   has_one :owner, through: :flats
+  has_many :review, dependent: :destroy
+
 
   validates :end_date, presence: true, date: { after_or_equal_to:  :start_date }
   validates :start_date, presence: true
